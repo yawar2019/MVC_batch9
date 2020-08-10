896afe323ca0bdb738cbf5ac444fa9cd3ee48056
yawar2019 <yawarali17@gmail.com>
2020-08-10 22:12:00 +0530

    Committed Code

--- CodeFirstApproach/Controllers/EmployeeController.cs
+++ CodeFirstApproach/Controllers/EmployeeController.cs
@@ -10,10 +10,20 @@ namespace CodeFirstApproach.Controllers
     public class EmployeeController : Controller
     {
         public EmployeeContext db = new EmployeeContext();
+        StudentEntities dbstud = new StudentEntities();
         // GET: Employee
         public ActionResult Index()
         {
             return View(db.EmployeeModel.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult HtmlHelperExample()
+        {
+            EmployeeModel obj = new Models.EmployeeModel();
+            obj.EmpName = "Suba";
+            ViewBag.Student = new SelectList(dbstud.StudentDetails, "StudId", "StudName",2);
+            return View(obj);
         }
     }
 }
